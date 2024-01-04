7f34166f52ccde7f8cbe020fa4ebef97856b0c7e
Nagaprasath <arjun141296@gmail.com>
2024-01-04 17:44:58 +0530

    Doctor Call Cell Added

--- E-Detailing/View/DoctorCallCell.swift
+++ E-Detailing/View/DoctorCallCell.swift
@@ -18,12 +18,25 @@ class DoctorCallCell: UICollectionViewCell {
     @IBOutlet weak var lblTownName: UILabel!
     
     
+    @IBOutlet weak var btnTownName: UIButton!
+    
+    
     var CallDetail : CallViewModel! {
         didSet{
             self.lblName.text = CallDetail.name
             self.lblTownName.text = CallDetail.townName
             self.lblCategory.text = CallDetail.categoryName
             self.lblSpecialty.text =  CallDetail.specialityName
+            
+            
+            self.btnTownName.contentEdgeInsets = UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10)
+            
+            self.btnTownName.setTitle(CallDetail.townName, for: .normal)
+            
+            self.btnTownName.backgroundColor = UIColor(red: CGFloat(241.0/255.0), green: CGFloat(83.0/255.0), blue: CGFloat(110.0/255.0), alpha: CGFloat(0.2))
+            
+            
+            
             
             
 //            if DCRType.doctor == CallDetail.type || DCRType.unlistedDoctor == CallDetail.type {
